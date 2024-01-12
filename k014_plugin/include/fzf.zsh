442514d88140e262d67c95542b40453d9df0f45b
Joe Cabezas Campos <joe.cabezas@gmail.com>
2024-01-12 03:42:32 -0800

    added fzf options

--- k014_plugin/include/fzf.zsh
+++ k014_plugin/include/fzf.zsh
@@ -3,3 +3,9 @@
 # export FZF_DEFAULT_OPTS="--height 60% --border --inline-info"
 # export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# Use ~~ as the trigger sequence instead of the default **
+export FZF_COMPLETION_TRIGGER='``'
+
+# Options to fzf command
+export FZF_COMPLETION_OPTS='--border --info=inline'
