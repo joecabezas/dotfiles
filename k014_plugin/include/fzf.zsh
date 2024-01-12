d4448ee08e1f4847bc78295fbb4fab37f2a54877
Joe Cabezas Campos <joe.cabezas@gmail.com>
2024-01-11 22:39:56 -0800

    now k014_plugin is loaded as a plugin by zplug

--- k014_plugin/include/fzf.zsh
+++ k014_plugin/include/fzf.zsh
@@ -1,3 +1,5 @@
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
 # export FZF_DEFAULT_OPTS="--height 60% --border --inline-info"
 # export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
