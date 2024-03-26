fdd3fd13e87005ffe9c9fb588f6d393f6a89fd9e
Joe Cabezas Campos <joe.cabezas@gusto.com>
2024-03-26 11:21:37 -0700

    updated neo-tree configs

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -51,7 +51,7 @@ return {
         icon = {
           folder_closed = "",
           folder_open = "",
-          folder_empty = "󰜌",
+          folder_empty = "󰉖",
         },
         modified = {
           symbol = "[+]",
@@ -65,14 +65,14 @@ return {
         git_status = {
           symbols = {
             -- Change type
-            added     = "A", -- or "✚", but this is redundant info if you use git_status_colors on the name
-            modified  = "M", -- or "", but this is redundant info if you use git_status_colors on the name
+            added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
             deleted   = "✖",-- this can only be used in the git_status source
             renamed   = "󰁕",-- this can only be used in the git_status source
             -- Status type
             untracked = "",
             ignored   = "",
-            unstaged  = "󰄱",
+            unstaged  = "",
             staged    = "",
             conflict  = "",
           }
@@ -167,8 +167,8 @@ return {
         filtered_items = {
           visible = false, -- when true, they will just be displayed differently than normal items
           hide_dotfiles = false,
-          hide_gitignored = true,
-          hide_hidden = true, -- only works on Windows for hidden files/directories
+          hide_gitignored = false,
+          hide_hidden = false, -- only works on Windows for hidden files/directories
           hide_by_name = {
             --"node_modules"
           },
