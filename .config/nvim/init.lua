9cc348eef013daa443c69d660fef816b7b017f08
Joe Cabezas Campos <joe.cabezas@gusto.com>
2024-03-27 03:21:14 -0700

    removing leader key mappings and coc, and more stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,2 @@
--- set runtimepath^=~/.vim runtimepath+=~/.vim/after
--- let &packpath = &runtimepath
--- source ~/.vimrc
-
-require('lazynvim')
-
 require('custom')
-require('leader-key-mappings')
---require('fzf')
-
--- Pending, I need a better way to load coc.vim config
--- vim.cmd "source ~/git/dotfiles/.vim/plugin/k014_custom_configs/coc.vim"
+require('lazynvim')
