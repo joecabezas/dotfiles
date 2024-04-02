23634224dc2950064482104af09bed642f1c6928
Joe Cabezas Campos <joe.cabezas@gusto.com>
2024-04-02 01:19:41 -0700

    auto formatted telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,21 +1,21 @@
 return {
-    'nvim-telescope/telescope.nvim',
-  tag = '0.1.6',
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.6",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
     {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-    }
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    },
   },
   keys = {
-    { '<leader>p', function() require('telescope.builtin').find_files() end, desc="Git Files"},
-    { '<leader>o', function() require('telescope.builtin').buffers() end, desc="Git Files"},
-    { '<leader>i', function() require('telescope.builtin').live_grep() end, desc="Git Files"},
+    { "<leader>p", function() require("telescope.builtin").git_files() end, desc = "Git Files", },
+    { "<leader>o", function() require("telescope.builtin").buffers() end, desc = "Buffers", },
+    { "<leader>i", function() require("telescope.builtin").live_grep() end, desc = "Live grep", },
   },
-  config = function()
-    require('telescope').setup({})
-    require('telescope').load_extension('fzf')
-  end
+  setup = function()
+    require("telescope").setup({})
+    require("telescope").load_extension("fzf")
+  end,
 }
