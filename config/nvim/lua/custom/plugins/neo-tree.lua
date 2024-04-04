e8b8fec38a9a48bfb5210a659cdc2c51e4fb9254
Joe Cabezas Campos <joe.cabezas@gusto.com>
2024-04-03 19:31:02 -0700

    add neo-tree

---
+++ config/nvim/lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,27 @@
+vim.keymap.set('n', '<leader>\\', '<cmd>Neotree reveal<CR>')
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
+    vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
+    vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
+    vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
+
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false, -- only works on Windows for hidden files/directories
+        },
+      },
+    }
+  end,
+}
