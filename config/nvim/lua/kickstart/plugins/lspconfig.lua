a47aef29842ddb8299201944edad138158f9bb4a
Joe Cabezas Campos <joe.cabezas@gmail.com>
2024-08-20 01:49:43 -0700

    remove sorbet from ensure_installed

--- config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -194,7 +194,7 @@ return {
       vim.list_extend(ensure_installed, {
         'eslint',
         'marksman',
-        'sorbet',
+        -- 'sorbet',
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
