46567c7092723a3b7d889abd18eea2f0f5f2d2c1
Joe Cabezas Campos <jcabezascampos@rippling.com>
2024-11-04 11:39:56 -0800

    added nvim-treesitter/nvim-treesitter-context

---
+++ config/nvim/lua/custom/plugins/nvim-treesitter-context.lua
@@ -0,0 +1,7 @@
+return {
+  'nvim-treesitter/nvim-treesitter-context',
+  opts = {
+    -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+    separator = nil,
+  },
+}
