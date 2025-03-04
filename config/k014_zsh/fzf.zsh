8f2744cf59b9f4f60a079e066b6a791dd1dea8e4
Joe Cabezas Campos <joe.cabezas@gmail.com>
2025-03-04 03:10:20 -0800

    update fzf.zsh

--- config/k014_zsh/fzf.zsh
+++ config/k014_zsh/fzf.zsh
@@ -1,11 +1,9 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-# export FZF_DEFAULT_OPTS="--height 60% --border --inline-info"
-# export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Use ~~ as the trigger sequence instead of the default **
-export FZF_COMPLETION_TRIGGER='``'
+# export FZF_COMPLETION_TRIGGER='``'
 
 # Options to fzf command
 export FZF_COMPLETION_OPTS='--border --info=inline'
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
