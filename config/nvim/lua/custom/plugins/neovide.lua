ed3d564e39531fc0cf6cd218366ce3f92e279886
Joe Cabezas Campos <jcabezascampos@rippling.com>
2025-03-04 03:50:11 -0800

    update neovide configuration

--- config/nvim/lua/custom/plugins/neovide.lua
+++ config/nvim/lua/custom/plugins/neovide.lua
@@ -5,13 +5,15 @@ if vim.g.neovide then
   vim.keymap.set('c', '<D-v>', '<C-r><C-o>+')
   vim.keymap.set('i', '<D-v>', '<C-r><C-o>+')
 
-  vim.o.guifont = 'JetBrainsMonoNL Nerd Font Mono'
+  vim.o.guifont = 'JetBrainsMono Nerd Font'
   vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_text_gamma = 0.1
+  vim.g.neovide_text_contrast = 0.0
 
-  vim.g.neovide_padding_top = 80
-  vim.g.neovide_padding_bottom = 80
-  vim.g.neovide_padding_right = 60
-  vim.g.neovide_padding_left = 60
+  vim.g.neovide_padding_top = 30
+  vim.g.neovide_padding_bottom = 30
+  vim.g.neovide_padding_right = 20
+  vim.g.neovide_padding_left = 20
 
   vim.g.neovide_show_border = false
   vim.g.neovide_refresh_rate = 120
